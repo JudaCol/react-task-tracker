1e3e5df6ff54ac8cfee36f49288c28c414d4d276
judacol <juliancolmenares806gmail.com>
2022-04-07 12:32:30 -0500

    task_tracker_react_tutorial / production_build_server_version

--- src/App.js
+++ src/App.js
@@ -1,64 +1,102 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import AddTask from "./components/addTask";
 // import Form from "./components/Form";
-
 
 
 // Importing a component from another js file on components directory
 const App = () => {
     const [showAddTask, setShowAddTask] = useState(false)
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Develop in REACT',
-            day: 'April 4th at 4:03 pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Study Solidity',
-            day: 'April 10th at 10:00 am',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Go to sleep',
-            day: 'April 15th at 10:00 pm',
-            reminder: true,
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() => {
+        // This is a kind of Promise to load the data from json server
+        const getTasks = async () => {
+            const tasksFromServer = await fetchTasks()
+            setTasks(tasksFromServer)
         }
-    ])
+        getTasks()
+    }, [])
+
+    // Fetch Tasks from server
+    const fetchTasks = async () => {
+    const res = await fetch('http://localhost:8000/tasks')
+    const data = await res.json()
+
+    return data
+    }
 
     //Delete Task
-    const deleteTask = (id) => {
-        setTasks(tasks.filter( task => task.id !== id))
+    const deleteTask = async (id) => {
+        await fetch(`http://localhost:8000/tasks/${id}`,
+            {method: 'DELETE',
+            })
+        setTasks(tasks.filter(task => task.id !== id))
         // console.log('delete', id)
     }
 
+    // Fetch Task from server (for DELETE)
+    const fetchTask = async (id) => {
+        const res = await fetch(`http://localhost:8000/tasks/${id}`)
+        const data = await res.json()
+
+        return data
+    }
+
+
     // Toggle Reminder
-    const toggleReminder = (id) => {
-        //console.log(id);
-        setTasks(tasks.map((task) => task.id === id ? {...task, reminder:
-                !task.reminder} : task
+    const toggleReminder = async (id) => {
+        // Toggle Reminder with server
+        const taskToToggle = await fetchTask(id)
+        const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+        const res = await fetch(`http://localhost:8000/tasks/${id}`, {
+            method : 'PUT',
+            headers: {
+                'Content-type' : 'application/json'
+            },
+            body: JSON.stringify(updTask)
+        })
+        const data = await res.json()
+
+        // Toggle Reminder without server
+        /* Note tha unique change that was made was on !task.reminder to data.reminder */
+        setTasks(tasks.map((task) =>
+                task.id === id ? {...task, reminder:
+                        data.reminder
+                } : task
             )
         )
 
     }
 
     // Add Task
-    const addTask = (task) => {
-        const id = Math.floor(Math.random() * 10000) + 1
-        const newTask = {id, ...task}
-        setTasks([...tasks, newTask])
+    const addTask = async (task) => {
+        // Add method without json-server
+        // const id = Math.floor(Math.random() * 10000) + 1
+        // const newTask = {id, ...task}
+        // setTasks([...tasks, newTask])
+
+        // Add method with json-server
+        // Initiate with the response (PROMISE)
+        const res = await fetch('http://localhost:8000/tasks', {
+            method : 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body : JSON.stringify(task)
+        })
+        const data = await res.json()
+        // Then add the data to the Tasks list finishing to the promise
+        setTasks([...tasks, data])
     }
 
     return (
         <div className='container'>
             <Header onAdd={() => setShowAddTask(!showAddTask)}
                     showAdd={showAddTask}/>
-            {showAddTask && <AddTask onAdd={addTask} />}
-            { tasks.length >0 ? <Tasks tasks={tasks} onDelete= {deleteTask} onToggle={toggleReminder}/> :
+            {showAddTask && <AddTask onAdd={addTask}/>}
+            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> :
                 'No tasks to show'
             }
         </div>
